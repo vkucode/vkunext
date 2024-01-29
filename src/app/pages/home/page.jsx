f32dd3272bfdc5d169134208ce0c784285973409
VKU CODE <ungurean729@gmail.com>
2024-01-29 15:13:22 +0100

    firstUpdate

---
+++ src/app/pages/home/page.jsx
@@ -0,0 +1,23 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import styles from './home.module.scss'
+
+const HomePage = () => {
+  return (
+    <header className={styles.header}>
+        <div className={styles.overlayVideo}></div>
+        <video src="/vkucodeEntryVideo.mp4.mp4" className={styles.videoHeader} autoPlay loop muted></video>
+        <div className={styles.titleHeaderCenter}>
+
+            {/* <Image src='/logoNOBG.png' width={200} height={200} /> */}
+            <h1>VKU CODE</h1>
+            <span>for us the sky is not the limit</span>
+        </div>
+        <div className={styles.subHeader}>
+          <h2>Paris based Digital Design Agency</h2>
+        </div>
+    </header>
+  )
+}
+export default HomePage;
